e749ef85d8d92f35cf35109fa99c978a098248d2
Nurul Baeti <nurulbaeti892@gmail.com>
2026-04-05 23:12:33 +0700

    perubahan

--- Labsession3/main_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.report_list, name='report_list'),
-    path('add/', views.add_report, name='add_report'),
-    path('edit/<int:id>/', views.edit_report, name='edit_report'),
-    path('delete/<int:id>/', views.delete_report, name='delete_report'),
-]
