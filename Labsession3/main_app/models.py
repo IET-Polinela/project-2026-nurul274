bcd587fe3cfc2074e405dbe42c1d17f6f0d12745
nurulbaeti <nurulbaeti892@gmail.com>
2026-04-07 22:52:31 +0700

    Update models.py

--- Labsession3/main_app/models.py
+++ Labsession3/main_app/models.py
@@ -5,10 +5,7 @@ class Report(models.Model):
     category = models.CharField(max_length=100)
     description = models.TextField()
     location = models.CharField(max_length=200)
-    status = models.CharField(
-        max_length=20,
-        default='REPORTED'
-    )
+    status = models.CharField(max_length=20, default='REPORTED')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
