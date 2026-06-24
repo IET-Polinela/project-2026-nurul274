2ab3a9df9002ca907d02257cbb9d02ccc5284367
nurul <nurulbaeti892@gmail.com>
2026-06-24 09:58:52 +0700

    Add GitHub Pages deployment

--- npm24782058_iet_2026/npm_iet_2026/npm_iet_2026/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Selamat Datang Nurul - 24782058 Teknologi Rekayasa Internet")
--- npm24782058_iet_2026/usermanagement_24782058/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import CustomLoginView, CustomLogoutView, register_view
-
-urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView, name='logout'), 
-    path('register/', register_view, name='register'),
-]
